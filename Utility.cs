7d2ede4bf891c3b90626b1681bfbaed51e829900
Armen <rmn.hovsepian@live.com>
2020-12-04 10:02:55 +0400

    calculate age

--- Utility.cs
+++ Utility.cs
@@ -1,5 +1,14 @@
 public class Utility
 {
-          static Double CalculateCompoundInterest(Double principle, Double interestRate, int noOfYears) 
-            => (principle) * Math.Pow((1 + (interestRate) / 100), noOfYears);
+    public static Double CalculateCompoundInterest(Double principle, Double interestRate, int noOfYears)
+      => (principle) * Math.Pow((1 + (interestRate) / 100), noOfYears);
+
+    public static int CalculateAge(DateTime dateOfBirth)
+    {
+        int calculatedAge = DateTime.Today.Year - dateOfBirth.Year;
+        if (dateOfBirth > DateTime.Today.AddYears(-calculatedAge))
+            calculatedAge--;
+        
+        return calculatedAge;
+    }
 }
